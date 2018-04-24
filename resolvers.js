812bc173e331fd0071236015fa12dc9e9fd4989e
Gusti Mahendra <gusti.mahendra@geekseat.com.au>
2018-04-24 14:33:51 +0800

    Added mutation graphql

--- resolvers.js
+++ resolvers.js
@@ -1,25 +1,28 @@
 var { mongoose } = require("./db/moongose");
 const { Post } = require("./models/Post");
 
-// const mypost = new Post({
-//   title: "merdeka indonesia",
-//   content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-//   createdAt: new Date(),
-//   modifiedAt: new Date(),
-//   author: "Gusti Mahendra"
-// });
-
-// mypost.save()
-
 const resolvers = {
   Query: {
-    posts: (obj, args, context) => {
-      return Post.find().then(
+    posts: () =>
+      Post.find().then(
         _posts => _posts,
         e => {
           console.log("Error:", e);
         }
-      );
+      )
+  },
+  Mutation: {
+    createPost: (obj, args, context) => {
+      const mypost = new Post({
+        title: args.input.title,
+        content: args.input.content,
+        createdAt: new Date(),
+        modifiedAt: new Date(),
+        author: args.input.author
+      });
+
+      mypost.save();
+      return mypost;
     }
   }
 };
--- typedefs.js
+++ typedefs.js
@@ -1,6 +1,8 @@
 const typeDefs = `
   type Query { posts: [Post] }
-  type Post { title: String!, content: String, createdAt: String, modifiedAt: String, author: String }
+  type Mutation { createPost(input: PostInput): Post }
+  type Post { id: String!, title: String!, content: String, createdAt: String, modifiedAt: String, author: String }
+  input PostInput { title: String!, content: String, author: String }
 `;
 
 module.exports = { typeDefs };
