2759c20c749ad99435521e260eb781c1255d07fa
Gusti Mahendra <gusti.mahendra@geekseat.com.au>
2018-04-27 11:17:44 +0800

    updated test case

--- tests/api/post.test.js
+++ tests/api/post.test.js
@@ -1,119 +1,187 @@
-const request = require('supertest');
-const { graphQLServer } = require('../server');
-const { MongoClient } = require('mongodb');
+const request = require("supertest");
+const { graphQLServer, serverListening } = require("../server");
+const { MongoClient, ObjectID } = require("mongodb");
+const mongoose = require("mongoose");
+const casual = require("casual");
 
 let connection;
 let db;
+const mongooseOpts = {
+  // options for mongoose 4.11.3 and above
+  autoReconnect: true,
+  reconnectTries: Number.MAX_VALUE,
+  reconnectInterval: 1000
+};
 
 beforeAll(async () => {
   connection = await MongoClient.connect(global.__MONGO_URI__);
   db = await connection.db(global.__MONGO_DB_NAME__);
+  mongoose.Promise = global.Promise;
+  mongoose.connect(global.__MONGO_URI__, mongooseOpts);
+  mongoose.connection.on("error", e => {
+    if (e.message.code === "ETIMEDOUT") {
+      console.log(e);
+      mongoose.connect(global.__MONGO_URI__, mongooseOpts);
+    }
+    console.log(e);
+  });
+
+  mongoose.connection.once("open", () => {
+    console.log(`MongoDB successfully connected to ${global.__MONGO_URI__}`);
+  });
 });
 
 afterAll(async () => {
   await connection.close();
   await db.close();
+  serverListening.close();
 });
 
-describe('Post', () => {
-  it('test js', async done => {
-    const users = db.collection('users');
+describe("Post", () => {
+  describe("Query", () => {
+    it("should query post(id) { ... } return single post", async done => {
+      const id = new ObjectID();
+      const post = db.collection("post");
 
-    const mockUser = { _id: 'some-user-id', name: 'John' };
-    await users.insertOne(mockUser);
+      const mockPost = {
+        _id: id,
+        title: casual.title,
+        content: casual.text,
+        createdAt: new Date(),
+        modifiedAt: new Date(),
+        author: casual.name
+      };
+      await post.insertOne(mockPost);
 
-    const insertedUser = await users.findOne({ _id: 'some-user-id' });
-    expect(insertedUser).toEqual(mockUser);
-  });
-
-  describe('Query', () => {
-    it('should query post(id) { ... } return single post', done => {
       request(graphQLServer)
-        .post('/graphql')
-        .set('Accept', 'application/json')
+        .post("/graphql")
+        .set("Accept", "application/json")
         .send({
           query:
-            'query getPost($id: String!){ post(id: $id) {id, title, content, createdAt, modifiedAt, author } }',
-          variables: { id: '5ae001f1aa2c9602700de0d1' }
+            "query getPost($id: String!){ post(id: $id) {id, title, content, createdAt, modifiedAt, author } }",
+          variables: { id: id.toHexString() }
         })
         .expect(200)
         .expect(response => {
-          expect(response.body.data.post).toBeNull();
+          expect(response.body.data.post).toEqual(mockPost);
         })
         .end(done);
     });
 
-    it('should query posts { ... } return array post', done => {
+    it("should query posts { ... } return array post", async done => {
+      const id = new ObjectID();
+      const post = db.collection("post");
+
+      const mockPost = {
+        _id: id,
+        title: casual.title,
+        content: casual.text,
+        createdAt: new Date(),
+        modifiedAt: new Date(),
+        author: casual.name
+      };
+      await post.insertOne(mockPost);
+
       request(graphQLServer)
-        .post('/graphql')
-        .set('Accept', 'application/json')
+        .post("/graphql")
+        .set("Accept", "application/json")
         .send({
-          query: '{posts{id, title, content, createdAt, modifiedAt, author }}'
+          query: "{posts{id, title, content, createdAt, modifiedAt, author }}"
         })
         .expect(200)
         .expect(response => {
-          expect(response.body.data.posts).toHaveLength(0);
+          expect(response.body.data.posts).toContainEqual(mockPost);
         })
         .end(done);
     });
   });
 
-  describe('Mutation', () => {
-    it('should mutation createPost(input) { ... } create new post', done => {
+  describe("Mutation", () => {
+    it("should mutation createPost(input) { ... } create new post", done => {
+      const mockPost = {
+        title: casual.title,
+        content: casual.text,
+        author: casual.name
+      };
+
       request(graphQLServer)
-        .post('/graphql')
-        .set('Accept', 'application/json')
+        .post("/graphql")
+        .set("Accept", "application/json")
         .send({
           query:
-            'mutation createPost($input: PostInput!) { createPost(input: $input) { id, title, content, createdAt, modifiedAt, author } }',
+            "mutation createPost($input: PostInput!) { createPost(input: $input) { title, content, author } }",
           variables: {
-            input: {
-              title: 'hello world',
-              content: 'hello there',
-              author: 'hendra'
-            }
+            input: mockPost
           }
         })
         .expect(200)
         .expect(response => {
-          expect(response.body.data.createPost).toBeNull();
+          expect(response.body.data.createPost).toEqual(mockPost);
         })
         .end(done);
     });
-    it('should mutation updatePost(id, input) { ... } update existing post', done => {
+    it("should mutation updatePost(id, input) { ... } update existing post", async done => {
+      const id = new ObjectID();
+      const post = db.collection("post");
+
+      const mockPost = {
+        _id: id,
+        title: casual.title,
+        content: casual.text,
+        createdAt: new Date(),
+        modifiedAt: new Date(),
+        author: casual.name
+      };
+      await post.insertOne(mockPost);
+
+      const mockUpdatedPost = {
+        title: "new title",
+        content: "new content",
+        author: "new author"
+      };
+
       request(graphQLServer)
-        .post('/graphql')
-        .set('Accept', 'application/json')
+        .post("/graphql")
+        .set("Accept", "application/json")
         .send({
           query:
-            'mutation updatePost($id: String!, $input: PostUpdateInput!){ updatePost(id: $id, input: $input) {id, title, content, author } }',
+            "mutation updatePost($id: String!, $input: PostUpdateInput!){ updatePost(id: $id, input: $input) {id, title, content, author } }",
           variables: {
-            id: '5adff09eb41347345cb6132c',
-            input: {
-              title: 'from zero to advanced',
-              content: 'this is content has updated',
-              author: 'hendra things'
-            }
+            id: id.toHexString(),
+            input: mockUpdatedPost
           }
         })
         .expect(200)
         .expect(response => {
-          expect(response.body.data.updatePost).toBeNull();
+          expect(response.body.data.updatePost).toEqual(mockUpdatedPost);
         })
         .end(done);
     });
-    it('should mutation deletePost(id) { ... } delete existing post', done => {
+    it("should mutation deletePost(id) { ... } delete existing post", async done => {
+      const id = new ObjectID();
+      const post = db.collection("post");
+
+      const mockPost = {
+        _id: id,
+        title: casual.title,
+        content: casual.text,
+        createdAt: new Date(),
+        modifiedAt: new Date(),
+        author: casual.name
+      };
+      await post.insertOne(mockPost);
+
       request(graphQLServer)
-        .post('/graphql')
-        .set('Accept', 'application/json')
+        .post("/graphql")
+        .set("Accept", "application/json")
         .send({
           query:
-            'mutation deletePost($id: String!){ deletePost(id: $id) {id, title, content, createdAt, modifiedAt, author } }',
-          variables: { id: '5adff09eb41347345cb6132c' }
+            "mutation deletePost($id: String!){ deletePost(id: $id) {id, title, content, createdAt, modifiedAt, author } }",
+          variables: { id: id.toHexString() }
         })
         .expect(200)
         .expect(response => {
-          expect(response.body.data.deletePost).not.toBeNull();
+          expect(response.body.data.deletePost).toEqual(mockPost);
         })
         .end(done);
     });
--- tests/server.js
+++ tests/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { graphqlExpress, graphiqlExpress } = require("apollo-server-express");
-const { mongoose } = require("../db/mongoose.testdb");
 const { schema } = require("../schema/schema");
 const GRAPHQL_PORT = 4001;
 const graphQLServer = express();
@@ -9,10 +8,10 @@ const graphQLServer = express();
 graphQLServer.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
 graphQLServer.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
 
-graphQLServer.listen(GRAPHQL_PORT, () =>
+const serverListening = graphQLServer.listen(GRAPHQL_PORT, () =>
   console.log(
     `GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`
   )
 );
 
-module.exports = { graphQLServer };
+module.exports = { graphQLServer, serverListening };
